e46c415a63032826c816b957be6172275ffc83b3
ZhaorRui <zhaorui623@qq.com>
2015-09-03 22:52:31 +0800

    第一步点击导入时，完成了内存中全局拓扑图的构造

---
+++ GuaranteeCircle/src/cn/gov/cbrc/sd/dz/zhaorui/model/Corporation.java
@@ -0,0 +1,96 @@
+package cn.gov.cbrc.sd.dz.zhaorui.model;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Corporation {
+
+	public static final String NAME_COL = "客户名称";
+
+	// 字段名->字段值
+	private LinkedHashMap<String, Object> datas;
+
+	private static Map<String, Corporation> corps;
+
+	public Corporation(LinkedHashMap<String, Object> datas) {
+		this.datas = datas;
+	}
+
+	public static void addCorp(Corporation corp) {
+		if (corps == null)
+			corps = new HashMap<String, Corporation>();
+		corps.put(corp.getStringValue(NAME_COL), corp);
+	}
+
+	public static void initCorps() {
+		if (corps != null)
+			corps.clear();
+	}
+
+	public String getStringValue(String key) {
+		return String.valueOf(datas.get(key));
+	}
+
+	public int getIntegerValue(String key) {
+		int value = 0;
+		try {
+			value = Integer.parseInt(getStringValue(key));
+		} catch (NumberFormatException e) {
+		}
+		return value;
+	}
+
+	public double getDoubleValue(String key) {
+		double value = 0;
+		try {
+			value = Double.parseDouble(getStringValue(key));
+		} catch (NumberFormatException e) {
+		}
+		return value;
+	}
+
+	public static void printCorps() {
+		if (corps == null) {
+			System.out.println("Corporation.corps is null");
+		} else {
+			System.out.println("Corporation.corps={");
+			Set<String> keys = corps.keySet();
+			for (String key : keys)
+				System.out.println(key + "->" + corps.get(key));
+			System.out.println("}");
+		}
+	}
+
+	public String toString() {
+		StringBuffer str = new StringBuffer("[");
+		Set<String> keys = datas.keySet();
+		for (String key : keys) {
+			str.append(key + "=" + datas.get(key) + "\t");
+		}
+		str.append("]");
+		return str.toString();
+	}
+
+	public static Collection<Corporation> getAllCorps() {
+		return corps.values();
+	}
+
+	public static Corporation getCorpByName(String name) {
+		if (corps != null)
+			return corps.get(name);
+		else
+			return null;
+	}
+
+	public static Corporation createDefaultCorp(String corpName) {
+
+		LinkedHashMap<String, Object> datas = new LinkedHashMap<String, Object>();
+		datas.put(NAME_COL, corpName);
+		Corporation corp = new Corporation(datas);
+		return corp;
+	}
+}
--- GuaranteeCircle/src/cn/gov/cbrc/sd/dz/zhaorui/module/impl/step1/Step1Module.java
+++ GuaranteeCircle/src/cn/gov/cbrc/sd/dz/zhaorui/module/impl/step1/Step1Module.java
@@ -3,7 +3,10 @@ package cn.gov.cbrc.sd.dz.zhaorui.module.impl.step1;
 import java.awt.Component;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.lang.model.type.ExecutableType;
 import javax.swing.JMenuItem;
@@ -11,12 +14,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import cn.gov.cbrc.sd.dz.zhaorui.GC;
+import cn.gov.cbrc.sd.dz.zhaorui.model.Corporation;
+import cn.gov.cbrc.sd.dz.zhaorui.model.TotalGraphic;
 import cn.gov.cbrc.sd.dz.zhaorui.module.Module;
+import jxl.Sheet;
 import jxl.Workbook;
 
 public class Step1Module extends Module {
 
-	private File customerInfoFile, guaranteeInfoFile;
+	private File customerInfoFile = new File("D:\\TDDOWNLOAD\\eclipse\\githome\\GuaranteeCircle\\客户信息表201506.xls"),
+			guaranteeInfoFile = new File("D:\\TDDOWNLOAD\\eclipse\\githome\\GuaranteeCircle\\担保关系表201506.xls");
 	private Workbook guaranteeInfoBook, customerInfo;
 
 	private DataSourcePanel cmPanel;
@@ -71,9 +78,66 @@ public class Step1Module extends Module {
 		else {
 			guaranteeInfoBook = Workbook.getWorkbook(guaranteeInfoFile);
 			customerInfo = Workbook.getWorkbook(customerInfoFile);
+
+			// 导入所有“客户信息表”中所有客户到内存中，形成“客户对象总表”
+			initCorps(customerInfo);//存储在Corportion.corps里
+			//在内存构建整张担保关系拓扑图对象
+			initGraphic(Corporation.getAllCorps(),guaranteeInfoBook);//存储在TotalGraphic.graphic里
 			return true;
 		}
 		return false;
+	}
+
+	private void initGraphic(Collection<Corporation> corps, Workbook book) {
+		TotalGraphic.init();
+		//解析“担保关系表”
+		Sheet sheet=book.getSheet(0);
+		int rowCount = sheet.getRows();
+		for (int row = 1; row < rowCount; row++) {
+				String corpName1=sheet.getCell(0,row).getContents();//担保人
+				String corpName2=sheet.getCell(1,row).getContents();//借款人
+				Corporation corp1=Corporation.getCorpByName(corpName1);//担保人
+				if(corp1==null){//说明担保人未在银行办理过信贷类业务
+					//则新建一个Corporation对象，并使用默认值初始化之，并加入到Corporation.corps中
+					corp1=Corporation.createDefaultCorp(corpName1);
+					Corporation.addCorp(corp1);
+				}
+				Corporation corp2=Corporation.getCorpByName(corpName2);//借款人
+				if(corp2==null){//说明借款人未在银行办理过信贷类业务----------------很奇怪，查查原因---------------
+					//则新建一个Corporation对象，并使用默认值初始化之，并加入到Corporation.corps中
+					corp2=Corporation.createDefaultCorp(corpName2);
+					Corporation.addCorp(corp2);
+				}
+				//将担保人和借款人以及其担保关系加入总图中
+				System.out.println(corp1+","+corp2);
+				TotalGraphic.addVertex(corp1);
+				TotalGraphic.addVertex(corp2);
+				TotalGraphic.addEdge(corp1,corp2);
+		}
+		TotalGraphic.printBasicInfo();
+	}
+
+	private void initCorps(Workbook book) {
+		Corporation.initCorps();
+		//解析“客户信息表”
+		Sheet sheet = book.getSheet(0);
+		int rowCount = sheet.getRows(), colCount = sheet.getColumns();
+		String colNames[] = new String[colCount];
+		// 取所有列名，形成数组
+		for (int col = 0; col < colCount; col++)
+			colNames[col] = sheet.getCell(col, 0).getContents();
+		// 取每一行数据，组成Corporation对象，加入到corps里
+		for (int row = 1; row < rowCount; row++) {
+			Corporation corp;
+			LinkedHashMap<String, Object> datas = new LinkedHashMap<String, Object>();
+			for (int col = 0; col < colCount; col++) {
+				String key = colNames[col];
+				Object value = sheet.getCell(col, row).getContents();
+				datas.put(key, value);
+			}
+			corp = new Corporation(datas);
+			Corporation.addCorp(corp);
+		}		
 	}
 
 }
